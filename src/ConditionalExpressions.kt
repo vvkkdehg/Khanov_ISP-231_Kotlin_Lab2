60720b2eec108628e92aba233897b22d9ededb67
Vlados <hanovvladik@mail.ru>
2025-09-26 00:23:21 +0300

    Create Programm

--- src/ConditionalExpressions.kt
+++ src/ConditionalExpressions.kt
@@ -41,38 +41,4 @@ fun main() {
     } else {
         println("Доступ запрещён!")
     }
-
-
-    print("Введите порядковый номер месяца (1-12): ")
-    val index = readln().toInt()
-    var month = ""
-    if (index == 1){
-        month = "Январь"
-    }
-    else if (index == 2){
-        month = "Февраль"
-    } else if (index == 3){
-        month = "Март"
-    } else if (index == 4){
-        month = "Апрель"
-    } else if (index == 5){
-        month = "Май"
-    } else if (index == 6){
-        month = "Июнь"
-    } else if (index == 7){
-        month = "Июль"
-    } else if (index == 8){
-        month = "Август"
-    } else if (index == 9){
-        month = "Сентябрь"
-    } else if (index == 10){
-        month = "Октябрь"
-    } else if (index == 11){
-        month = "Ноябрь"
-    } else if (index == 12){
-        month = "Декабрь"}
-    if (index in 1..12){
-        println("Вы ввели порядковый номер $index, ему соответсвует месяц $month.")
-    }
-
 }
